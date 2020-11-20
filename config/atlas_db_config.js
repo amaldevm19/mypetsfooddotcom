bf872c667f9d769388129220ca2a8114bb5fd3fd
Amaldev <amaldevm19@gmail.com>
2020-11-20 16:35:44 +0400

    updated db configuration

--- config/atlas_db_config.js
+++ config/atlas_db_config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-exports.db = mongoose.connect('mongodb+srv://amaldevm19:KMahadevAn@cluster0.i38xu.mongodb.net/mypetsfooddotcom?retryWrites=true&w=majority', {useNewUrlParser: true},(err)=>{
+exports.db = mongoose.connect('mongodb+srv://amaldevm19:KMahadevAn@cluster0.i38xu.mongodb.net/mypetsfooddotcom?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology: true},(err)=>{
     if (err) throw err;
     console.log('Successfully connected to MongoDB');
 });
